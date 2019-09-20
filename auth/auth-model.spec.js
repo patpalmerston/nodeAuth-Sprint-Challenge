33c6176c9c76e057465f1f1cf046d2d61e054cfc
Pat Palmerston <patpalmerston@gmail.com>
2019-09-20 09:08:31 -0700

    model testing

--- auth/auth-model.spec.js
+++ auth/auth-model.spec.js
@@ -9,4 +9,25 @@ describe('users model', () => {
 	it('should set environment to testing', () => {
 		expect(process.env.DB_ENV).toBe('testing');
 	});
+
+	describe('add()', () => {
+		it('should add User into the db', async () => {
+			await Users.add({ username: 'Pat', password: 'Pat' });
+
+			let users = await db('users');
+			// console.log(users);
+			expect(users).toHaveLength(1);
+		});
+	});
+
+	describe('add()', () => {
+		it('should add User into the db', async () => {
+			await Users.add({ username: 'Pat', password: 'Pat' });
+			await Users.add({ username: 'Kel', password: 'Kel' });
+
+			let users = await db('users');
+			// console.log(users);
+			expect(users).toHaveLength(2);
+		});
+	});
 });
