814c720d2f337f6ad3b0b5d71efabbaa59220d89
Pat Palmerston <patpalmerston@gmail.com>
2019-09-20 08:52:57 -0700

    login and authentication for jokes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,19 +4,17 @@ const tokenService = require('./token-service');
 const Users = require('./auth-model');
 
 router.post('/register', (req, res) => {
-  let { username, password } = req.body;
-  console.log(req.body)
+	let { username, password } = req.body;
+	console.log(req.body);
 	const hash = bcrypt.hashSync(password, 8);
 
 	Users.add({ username, password: hash })
 		.then(saved => {
 			const token = tokenService.generateToken(saved);
-			res
-				.status(201)
-				.json({
-					message: `Hello ${saved.username} you are registered!`,
-					token
-				});
+			res.status(201).json({
+				message: `Hello ${saved.username} you are registered!`,
+				token
+			});
 		})
 		.catch(err => {
 			console.log(err);
@@ -25,7 +23,25 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-	// implement login
+	let { username, password } = req.body;
+
+	Users.findBy({ username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = tokenService.generateToken(user);
+
+				return res
+					.status(200)
+					.json({ message: `Welcome ${user.username}!`, token });
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'could not find user' });
+		});
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,6 +2,8 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
 
 module.exports = (req, res, next) => {
 	const token = req.headers.authorization;
